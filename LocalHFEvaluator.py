6f02d4752eb28d96dec23dacb692b8aa7da700a2
geronimi73 <141400217+geronimi73@users.noreply.github.com>
2024-01-08 10:04:16 +0100

    Create LocalHFEvaluator.py

---
+++ LocalHFEvaluator.py
@@ -0,0 +1,18 @@
+import os
+import tiktoken
+from LLMNeedleHaystackTester import LLMNeedleHaystackTester
+
+
+class LocalHFEvaluator(LLMNeedleHaystackTester):
+
+
+# if __name__ == "__main__":
+#     # Tons of defaults set, check out the LLMNeedleHaystackTester's init for more info
+#     ht = AnthropicEvaluator(model_name='claude-2.1', evaluation_method='gpt4')
+
+#     ht.start_test()
+
+
+
+
+
